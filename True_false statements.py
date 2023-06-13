8b785dd80dfade0e861a1be821293253b76470f0
Ayiekoo <131998667+Ayiekoo@users.noreply.github.com>
2023-06-14 02:31:48 +0300

    Add files via upload

---
+++ True_false statements.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+### False, True statements
+print(1 > 2)
+
+
+# In[2]:
+
+
+print(2 < 1)
+
+
+# In[3]:
+
+
+print(2 > 1)
+
+
+# In[ ]:
+
+
+
+
